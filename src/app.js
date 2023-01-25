277768bd30c95f4880270bddc6b22545773b9a38
Allison <a.velardep@pucp.edu.pe>
2023-01-25 14:20:19 -0500

    create and app file

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const routerApi = require('./src/routes');
+const routerApi = require('./routes');
 
 function app() {
   const app = express();
